9a2be7d8c02047ec0716026bdea67276aefea3ae
Gabriel Ribeiro Guerra <gabriel.guerra@mindsdigital.net>
2021-09-27 11:46:11 -0300

    adicionando injectable no pubsub helper

--- projects/bmgmoney-tracking/src/utils/pubsub-helper.ts
+++ projects/bmgmoney-tracking/src/utils/pubsub-helper.ts
@@ -1,17 +1,16 @@
+import { Injectable } from "@angular/core";
 import { HttpService } from "./http-service"
 
 const pubsubUrl = "https://southamerica-east1-modern-index-321812.cloudfunctions.net/";
 const pubsubEndpoint = "event-digestion"
 
+@Injectable()
 export class PubsubHelper{
-    constructor(private httpService: HttpService){
-
-    }
+    constructor(private httpService: HttpService){ }
 
     publish(message: any){
         this.httpService.post(pubsubUrl, pubsubEndpoint, message).subscribe(result =>{
             console.log("Published to pubsub:" + JSON.stringify(result));
         });
     }
-
 }
