71e3415340257fc1f65fb9ab9b630ac7b3055549
Gabriel Ribeiro Guerra <gabriel.guerra@mindsdigital.net>
2021-09-27 14:16:49 -0300

    desabilitando ivy | adicionando http e common module

--- projects/bmgmoney-tracking/src/lib/bmgmoney-tracking.module.ts
+++ projects/bmgmoney-tracking/src/lib/bmgmoney-tracking.module.ts
@@ -2,9 +2,11 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { BmgMoneyTrackingConfig } from './bmgmoney-tracking.config';
 import { BmgmoneyTrackingService } from './bmgmoney-tracking.service';
 import { HttpService } from '../utils/http-service';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
+  declarations: [CommonModule, HttpClient ],
   imports: [],
   exports: []
 })
